3df262810b91e6ffd9020da1b28c2e19f2bef814
ZHOU Bin <bin.zhou@dyson.com>
2023-04-17 07:32:35 +0800

    update homebench vision

--- dreamerv3/embodied/envs/homebench.py
+++ dreamerv3/embodied/envs/homebench.py
@@ -7,40 +7,60 @@ import os
 import embodied
 import numpy as np
 
-
 class HomeBenchEmb(embodied.Env):
     def __init__(self, task, repeat=1, render=True, size=(64, 64), camera=-1):
         assert type(task) == str
-        hb_env = HomeBench("HomeBenchExample.ReachTarget", DeltaJointPosition())
+        hb_env = HomeBench("HomeBenchExample.ReachTarget", DeltaJointPosition(), episode_steps=1000)
         self._env = hb_env
 
     @functools.cached_property
     def obs_space(self):
-        spec = self._env.environment_spec
-        if "reward" in spec:
-            spec["obs_reward"] = spec.pop("reward")
+        spec = self._env.environment_spec.observations
         
         obs_spec = {}
         for k, v in spec.items():
-            
+            embodied.Space(str(v.dtype), v.shape)
+            obs_spec[k] = v
         
         return {
             "reward": embodied.Space(np.float32),
             "is_first": embodied.Space(bool),
             "is_last": embodied.Space(bool),
             "is_terminal": embodied.Space(bool),
-            **{k or self._obs_key: self._convert(v) for k, v in spec.items()},
+            **obs_spec,
         }
 
     @functools.cached_property
     def act_space(self):
-        pass
-
+        act_spec = self._env.environment_spec.actions
+        act_space = embodied.Space(str(act_spec.dtype), act_spec.shape, 
+                                   low=act_spec.low, high=act_spec.high)        
+        return {
+            "reset": embodied.Space(bool),
+            "action": act_space
+        }
+    
     def step(self, action):
-        pass
+        action = action.copy()
+        reset = action.pop("reset")
+        if reset or self._done:
+            time_step = self._env.reset()
+        else:
+            time_step = self._env.step(action)
+        time_step = time_step[0]
+        self._done = time_step.last()
+        return self._obs(time_step)
 
     def _obs(self, time_step):
-        pass
-
-    def _convert(self, space):
-        pass
+        if not time_step.first():
+            assert time_step.discount in (0, 1), time_step.discount
+        obs = time_step.observation
+        obs = dict(obs)
+        obs["obs_reward"] = time_step.reward()
+        return dict(
+            reward=np.float32(0.0 if time_step.first() else time_step.reward),
+            is_first=time_step.first(),
+            is_last=time_step.last(),
+            is_terminal=False if time_step.first() else time_step.discount == 0,
+            **obs,
+        )
